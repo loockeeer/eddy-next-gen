81301ef83d456a196995d559b8eccaff2f51dd7e
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2021-02-25 17:56:23 +0100

    fix: use all non privileged intents

--- eddy.js
+++ eddy.js
@@ -49,7 +49,7 @@ const talkcount = io.counter({
 })
 
 
-const client = new Discord.Client({ intents: ["GUILD_MESSAGES", "DIRECT_MESSAGES"] })
+const client = new Discord.Client({ intents: new Discord.Intents(Discord.Intents.NON_PRIVILEGED) })
 
 client.login(process.env.TOKEN)
 
